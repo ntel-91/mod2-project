d0503fb889ee8e57d061c140f7bf42286fd16d89
github username <github email address>
2019-10-01 17:19:23 -0400

    create views

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,15 +1,20 @@
 class EpisodesController < ApplicationController
-    def index
+    def index  
         @episodes = Episode.all
     end
   
     def show
+        @episode = Episode.find(params[:id])
     end
   
     def new
     end
   
     def create
+        @episode = Episode.find(params[:id])
+        @user = session[:user]
+        View.create(user_id: @episode.id, episode_id: @user["id"])
+        redirect_to episode_path(@episode)
     end
   
     def edit
--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -9,6 +9,10 @@ class ViewsController < ApplicationController
   end
 
   def create
+    @episode = params[:view][:episode]
+    @user_id = session[:user]["id"]
+    View.create(user_id: @user_id, episode_id: @episode)
+    redirect_to episode_path(@episode)
   end
 
   def edit
